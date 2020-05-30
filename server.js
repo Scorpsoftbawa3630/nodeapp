8c0a8acc047c0d76d8ad05a9f7e48c91e30fc45a
Scorpsoft GIT <manisha.scorpsoft@gmail.com>
2020-05-31 00:29:01 +0530

    this cahnge for hroku

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
-console.log('hello');
-console.log('rohit');
+Obj=require('express');
+const port=process.env.PORT || 3000;
+method=Obj();
+
+method.get('',(req,res)=>{
+	res.send('hey wollcome on live');
+});
+
+method.listen(port,()=>{
+	console.log('server strat');
+});
